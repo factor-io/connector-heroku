177b42c11368ec63626efcdbfe02f221261e7595
Maciej Skierkowski <maciej@skierkowski.com>
2014-11-24 17:51:58 -0800

    placeholder for new code

--- lib/factor/connector/heroku.rb
+++ lib/factor/connector/heroku.rb
@@ -13,5 +13,14 @@ Factor::Connector.service 'heroku' do
     fail 'No app specified. Where am I supposed to deploy it?' unless app
     fail 'You need to activate Heroku on the services page' unless api_key
 
+    base     = 'https://api.heroku.com'
+    app_base = "#{base}/apps/#{app}"
+    headers  = {
+      'Authorization' => "Bearer #{api_key}",
+      'Accept' => 'application/vnd.heroku+json; version=3',
+      'Content-Type' => 'application/json'
+    }
+
+    action_callback deploy_info:''
   end
 end
